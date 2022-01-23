a09f1a4d344b2cc769617cb9ef7ae36142e7fdb5
unnnndy <andy.ho23@ycdsbk12.ca>
2022-01-23 14:24:46 -0500

    mistake refix

--- Sketch.java
+++ Sketch.java
@@ -1,19 +1,20 @@
+
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
-  int counter = width/2;
+  int counter = 1;
   float height2 = height/2;
   double colour = 255/height2;
   double checker = 2.8;
 
   int difference = height/2;
   int tendiff = difference/10;
+  double angle = 0;
 
   public void settings() {
 	// put your size call here
-    size(800, 800);
-
+    size(500, 500);
   }
 
   public void setup() {
@@ -30,30 +31,9 @@ public class Sketch extends PApplet {
         line(i, height, i, (height/2)); 
 
         dark = dark + 1;
-        //System.out.println(counter);
-        //counter--;
         }
 
     //bottom left corner
-
-      for(int b = 8; b >= 1; b--){
-        stroke(0);
-        fill(255,190,0);
-        ellipse(height/1.33f,height/1.14f,height/16,height/6);
-        stroke(0);
-        fill(255,190,0);
-        ellipse(height/1.33f,height/1.6f,height/16,height/6);
-        stroke(0);
-        fill(255,190,0);
-        ellipse(height/1.6f,height/1.33f,height/6,height/16);
-        stroke(0);
-        fill(255,190,0);
-        ellipse(height/1.14f,height/1.33f,height/6,height/16);
-        }
-
-        stroke(0);
-        fill(150,75,0);
-        ellipse(height/1.33f,height/1.33f,height/10,height/10);
   
     //top left corner, lines
       for(int x = height/20; x <= height/2-(height/20);x += height/20){
@@ -79,9 +59,26 @@ public class Sketch extends PApplet {
         ellipse(circlex, (height/12)*5, height/20, height/20);
       }
       
+      stroke(0);
+      fill(150,75,0);
+      ellipse(height/1.33f,height/1.33f,height/10,height/10);
+
+      if (counter <= 2){
+      for (int i = 0; i < 8; i++){
+
+        push();
+        translate(375,375);
+        rotate((float)angle);
+        stroke(0);
+        fill(255,190,0);
+        ellipse(0,height/7.9f,height/16,height/6);
+        pop();
+
+        angle = angle + 0.785;
+        counter++;
+      }
+    }
+
 
   }
-
-  
-  // define other methods down here.
 }
